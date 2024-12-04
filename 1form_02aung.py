e85a1f66aec62600edd239f90735c26792dfeeaa
Peerapong <135480419+Peerapong25550@users.noreply.github.com>
2024-12-04 16:05:57 +0700

    Add files via upload

---
+++ 1form_02aung.py
@@ -0,0 +1,11 @@
+student = int(input("รหัสนักเรียน: "))
+nickname = input("ชื่อเล่น: ")
+age = int(input("อายุ: "))
+item = input("ต้องการยืมใช้อะไรดี: ")
+
+print("\n" + "=" * 50 + "\n" + "Welcome to Pre-Engineering & Robotics\n")
+print("สวัสดีครับ กรอกข้อมูลตนเองครับผม")
+print("ชื่อเล่น:", nickname)
+print("รหัสนักเรียน:", student)
+print("อายุ:", age, "ปี")
+print("ต้องการยืม:", item)
